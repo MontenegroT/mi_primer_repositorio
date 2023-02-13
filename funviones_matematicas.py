00add07766a5caaef6cdac29fc449fe556e64901
Tomas Montenegro <montenegrotomas33@gmail.com>
2023-02-13 13:24:32 -0300

    meti a los otros archivos

---
+++ funviones_matematicas.py
@@ -0,0 +1,13 @@
+def sumar (a,b):
+    print(f"la suma es igual {a+b}")
+
+def resta (a,b):
+    print(f"la suma es igual {a-b}")
+
+def multi (a,b):
+    print(f"la suma es igual {a*b}")
+
+def div (a,b):
+    print(f"la suma es igual {a/b}")
+
+sumar(4,5)
---
+++ uso.py
@@ -0,0 +1,4 @@
+import mi_primer_paquete.funviones_matematicas as funviones_matematicas
+
+
+funviones_matematicas.resta(4,4)
